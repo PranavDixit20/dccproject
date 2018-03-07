8eef6c7361f029c14b3f525101523845240d2581
Pranav Dixit <pranavdixit20@gmail.com>
2018-03-07 12:41:31 +0530

    logout and new dashboard ...

--- dccwebproject/loginapp/views.py
+++ dccwebproject/loginapp/views.py
@@ -12,9 +12,17 @@ def dash(request):
 def loggin(request):
         if request.method == 'POST':
             form = AuthenticationForm(data=request.POST)
-
             if form.is_valid():
                 return render(request,'loginapp/index.html')
             else:
                 form = AuthenticationForm()
                 return render(request,'loginapp/login.html',{'form':form})
+
+def logout(request):
+    return render(request,'loginapp/login.html')
+
+def dash2(request):
+    return render(request,'loginapp/index2.html')
+
+def dash1(request):
+    return render(request,'loginapp/index.html')
